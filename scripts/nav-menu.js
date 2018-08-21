52e7d7252b60c8cacc28c799e0eac6bb761ecead
neyney <neyney10@live.com>
2018-08-21 16:10:41 +0300

    added login-form into the nav-menu when out of user

--- scripts/nav-menu.js
+++ scripts/nav-menu.js
@@ -2,7 +2,6 @@ var nav_menu_show = false;
 var nav_menu_modal = document.getElementById('nav-menu-content');
 
 var navbar_account = document.getElementById('account');
-console.log(navbar_account);
 if(navbar_account !== null) 
 navbar_account.addEventListener('click',function(){
     if(nav_menu_show === false) {
@@ -20,4 +19,12 @@ nav_menu_btn_close.addEventListener('click',nav_menu_close);
 function nav_menu_close() {
     nav_menu_modal.style.display = 'none';
     nav_menu_show = false;
+}
+
+var nav_menu_item_login = document.getElementById("nav-menu-item-login");
+if(nav_menu_item_login !== null) {
+    nav_menu_item_login.addEventListener('click',function(){
+        document.getElementById('nav-menu-item-login-form').classList.toggle("hide-login");
+    });
+
 }
